7f2c6cf41500ef54589a25f1551bebc7549d912f
Tiago Ceccon <ceccon.t@hotmail.com>
2024-08-12 22:45:51 -0300

    Init repo

---
+++ JenAI/src/main/java/dev/ceccon/JenAI.java
@@ -0,0 +1,7 @@
+package dev.ceccon;
+
+public class JenAI {
+    public static void main(String[] args) {
+        System.out.println("Hello Human!");
+    }
+}
---
+++ JenAI/src/test/java/dev/ceccon/AppTest.java
@@ -0,0 +1,9 @@
+package dev.ceccon;
+
+public class AppTest
+{
+
+    public void shouldAnswerWithTrue() {
+    }
+
+}
