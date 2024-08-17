c86586089be4e2b203d3cbb724f609e1734ab9f2
Tiago Ceccon <ceccon.t@hotmail.com>
2024-08-17 13:53:28 -0300

    Add LLM API client

--- JenAI/CHANGELOG.md
+++ JenAI/CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Consume LLM API with fixed configuration (localhost, port 8080).
+
 ### Changed
 
 ### Deprecated
---
+++ JenAI/src/main/java/dev/ceccon/client/LLMClient.java
@@ -0,0 +1,50 @@
+package dev.ceccon.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.ceccon.dtos.PromptDTO;
+import dev.ceccon.dtos.ResponseDTO;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LLMClient {
+
+    public static ResponseDTO sendPrompt(String urlString, PromptDTO promptDTO) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        URL url = new URL(urlString);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+
+        String body = mapper.writeValueAsString(promptDTO);
+
+        try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
+            dos.writeBytes(body);
+            dos.flush();
+        }
+
+        int responseCode = connection.getResponseCode();
+        String rawResponse = "";
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            rawResponse = response.toString();
+        }
+
+        connection.disconnect();
+
+        return mapper.readValue(rawResponse, ResponseDTO.class);
+    }
+}
