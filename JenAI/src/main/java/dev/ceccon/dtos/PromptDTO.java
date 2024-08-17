fce87267f97d714c01acdd7503691b0ddbe8856e
Tiago Ceccon <ceccon.t@hotmail.com>
2024-08-16 22:55:59 -0300

    Add prompt DTO

---
+++ JenAI/src/main/java/dev/ceccon/dtos/PromptDTO.java
@@ -0,0 +1,36 @@
+package dev.ceccon.dtos;
+
+import dev.ceccon.conversation.Chat;
+import dev.ceccon.conversation.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromptDTO {
+
+    public record MessageDTO(String role, String content) {}
+
+    private String model = "";
+    private List<MessageDTO> messages = new ArrayList<>();
+
+    private PromptDTO() {}
+
+    public static PromptDTO forChat(Chat chat) {
+        PromptDTO prompt = new PromptDTO();
+
+        for (Message message : chat.getMessages()) {
+            prompt.messages.add(new MessageDTO(message.role(), message.content()));
+        }
+
+        return prompt;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public List<MessageDTO> getMessages() {
+        return messages;
+    }
+
+}
